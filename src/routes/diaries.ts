6bb85ba221fbdfcffcbd685b2bdd6250cb8a5b28
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-04-26 14:30:08 -0300

    Update routes and services

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,14 @@
 import express from 'express'
+import * as diaryServices from '../services/diary'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all entry diaries')  
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
 router.post('/', (_req, res) => {
-  res.send('Saving a diary!')  
+  res.send('Saving a diary!')
 })
 
 export default router
--- src/services/diary.ts
+++ src/services/diary.ts
@@ -1,5 +1,19 @@
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
-export const getEntries = () => diaryData
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
-export const addEntry = () => {}
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id,
+      date,
+      weather,
+      visibility
+    }
+  })
+}
+
+export const addEntry = (): undefined => undefined
