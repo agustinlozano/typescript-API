94819c59bc1b74be6cf2b068a0fef860a076b7f4
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-04-26 14:30:17 -0300

    Add build

---
+++ build/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/', (_req, res) => {
+    res.send('<h1>Hello, word!</h1>');
+});
+app.get('/ping', (_req, res) => {
+    console.log('Someone pinged here!');
+    res.send('pong');
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
